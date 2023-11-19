7e6f5bd3515a7d1fdfcce66559b18f9f3ab377d4
Swati Panchal <swati.panchal@pavansgroup.in>
2023-11-18 19:56:16 -0500

    Added constructor and getter setters

--- Employee.java
+++ Employee.java
@@ -1,2 +1,51 @@
+import java.time.Year;
+
 public class Employee {
+
+    private String name;
+    private int birthYear;
+    private int age;
+    private double monthlySalary;
+    private int rate;
+
+    public Employee(String name, int birthYear, double monthlySalary, int rate) {
+        this.name = name;
+        this.birthYear = birthYear;
+        this.age =  Year.now().getValue() - birthYear;
+        this.monthlySalary = monthlySalary;
+        this.rate = rate;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public double getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setMonthlySalary(double monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
 }
--- Manager.java
+++ Manager.java
@@ -1,2 +1,27 @@
-public class Manager {
+public class Manager extends Employee {
+
+    private float nbTravelDays;
+    private int nbClients;
+
+    public Manager(String name, int birthYear, double monthlySalary, int rate, float nbTravelDays, int nbClients) {
+        super(name, birthYear, monthlySalary, rate);
+        this.nbTravelDays = nbTravelDays;
+        this.nbClients = nbClients;
+    }
+
+    public float getNbTravelDays() {
+        return nbTravelDays;
+    }
+
+    public void setNbTravelDays(float nbTravelDays) {
+        this.nbTravelDays = nbTravelDays;
+    }
+
+    public int getNbClients() {
+        return nbClients;
+    }
+
+    public void setNbClients(int nbClients) {
+        this.nbClients = nbClients;
+    }
 }
