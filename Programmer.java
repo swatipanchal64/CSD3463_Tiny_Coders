14ced1bc64ae4b4bf5e6f4bfcf2c6545902cede4
Jayti01 <jaytipatel01@gmail.com>
2023-11-20 19:36:42 -0500

    created constructor and function for programmer class

--- Programmer.java
+++ Programmer.java
@@ -1,2 +1,16 @@
-public class Programmer {
+public class Programmer extends Employee {
+
+    private int nbProjects;
+
+    public Programmer(String name, int birthYear, double monthlySalary, int rate, int nbProjects) {
+        super(name, birthYear, monthlySalary, rate);
+        this.nbProjects = nbProjects;
+    }
+        public int nbProjects() {
+            return nbProjects;
+        }
+    
+        public void nbProjects(int nbProjects) {
+            this.nbProjects = nbProjects;
+        }   
 }
