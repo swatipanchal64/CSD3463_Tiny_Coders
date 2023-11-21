b33b35a11c6fac38e8f1d650aca57fdf98fca4c1
Jayti01 <jaytipatel01@gmail.com>
2023-11-20 19:39:38 -0500

    created constructor and function for tester class

--- Tester.java
+++ Tester.java
@@ -1,2 +1,16 @@
-public class Tester {
+public class Tester extends Employee{
+    
+    private int nbBugs;
+
+    public Tester(String name, int birthYear, double monthlySalary, int rate, int nbBugs) {
+        super(name, birthYear, monthlySalary, rate);
+        this.nbBugs = nbBugs;
+    }
+        public int nbBugs() {
+            return nbBugs;
+        }
+    
+        public void nbBugs(int nbBugs) {
+            this.nbBugs = nbBugs;
+        }
 }
