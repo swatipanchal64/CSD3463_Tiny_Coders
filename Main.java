76197483b8283bd06efe885cf26dbfe9c098a0d4
Swati Panchal <swati.panchal@pavansgroup.in>
2023-12-02 14:48:05 -0500

    Added main and management file

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Test part 1
+        System.out.println("Test 1: ");
+        var management = new Management();
+        management.hireTest1();
+
+        // Test part 2
+        System.out.println("Test 2: ");
+        System.out.println("Employees description: ");
+        management.hireTest2();
+
+        // Test part 3
+        System.out.println("Test 3: ");
+        System.out.println("Employees description: ");
+        management.hireTest3();
+    }
+}
